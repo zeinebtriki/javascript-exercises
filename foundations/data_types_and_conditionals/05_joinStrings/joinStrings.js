23f93bc1ab83264452d0d933b4b29876bc4fb7de
zeinebtriki <76732158+zeinebtriki@users.noreply.github.com>
2026-09-23 12:07:35 +0100

    solve exercice 05 of foundations/data-types..

--- foundations/data_types_and_conditionals/05_joinStrings/joinStrings.js
+++ foundations/data_types_and_conditionals/05_joinStrings/joinStrings.js
@@ -9,7 +9,14 @@
 
 // Add your code right below, good luck!
 
+const birthYear = 1947;
+const thisYear = 1965;
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const fullName = firstName+" " + lastName;
+const age = thisYear-birthYear;
 
+let greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
 
 
